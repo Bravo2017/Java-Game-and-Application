b571c8002e6e69eac7200109b23c469ab358db12
Shashank Singhal <shashanksinghal02@gmail.com>
2016-12-09 10:55:10 +0530

    Update and rename Color Picker.java to Color_Picker.java

--- Color_Picker.java
+++ Color_Picker.java
@@ -2,10 +2,10 @@
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-/*<applet code="Color Picker" width=500 height=450>
+/*<applet code="Color_Picker" width=500 height=450>
  </applet>
  */
-public class Color Picker extends Applet implements MouseListener
+public class Color_Picker extends Applet implements MouseListener
 { int i,j,k,z=0,y=0,x=0,a,x1,y1,r1,g1,b1,r2,g2,b2,x2,y2,h;
   TextField red,green,blue;
   public void init()
